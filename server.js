e0a56352875d1f3323005a7e80aecc3fb805db0c
cridder <cjridder@gmail.com>
2023-03-05 13:23:36 -0800

    wtf

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// 
-// requires
 const path = require("path");
 const express = require("express");
 const session = require("express-session");
@@ -7,20 +5,17 @@ const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 
-//  session requires
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-//  app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// handlebars custom helpers
+// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
-// session settings
 const sess = {
-	secret: "Zj&o*@*GjBbpvQssuwpcU%DSj@sDDJo7",
+	secret: "Super secret secret",
 	cookie: {
 		maxAge: 300000,
 		httpOnly: true,
@@ -36,10 +31,9 @@ const sess = {
 
 app.use(session(sess));
 
-// use this template engine
+// Inform Express.js on which template engine to use
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -47,8 +41,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-//  server port message start
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log("Now listening"));
 });
-

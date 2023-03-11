c40bea7e4e7d6e96fe3d5e294e4160fca623edd5
cridder <cjridder@gmail.com>
2023-03-11 15:19:58 -0800

    modifing handlebars and roms

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
+const dbInit = require("./config/dbInit");
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
+// initialize the database if it doesn't exist
+dbInit();
 const app = express();
 // const PORT = process.env.PORT || 3001;
 
